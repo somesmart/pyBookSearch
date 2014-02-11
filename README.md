b811c92928f471fdeff2377449220753f70f90cb
Chris Willoughby <chrrrisw@gmail.com>
2014-02-11 13:03:02 +1030

    updated README, removed commented code

--- README.md
+++ README.md
@@ -5,10 +5,19 @@ A simple terminal or GTK+ based interface to add books to a library file.
 Designed to aid entry of books by scanning or typing in their ISBN. A web search then looks up details
 and the ISBN, Title, and Author are added to a CSV file.
 
+Books may also be added manually.
+
+The interface is optimised for efficient repeated book entry.
+
+Some ancilliary routines for post-processing are also included.
+
 ## Dependencies
 
-*   BeautifulSoup
-*   gtk
-*   gobject
-*   urllib2
+*   bs4 (BeautifulSoup, SoupStrainer)
+*   collections
 *   csv
+*   gi.repository (Gtk, GObject)
+*   html (entities)
+*   json
+*   optparse (OptionParser)
+*   urllib (request, error, parse)
--- crwGTKScannerEntry.py
+++ crwGTKScannerEntry.py
@@ -21,19 +21,9 @@ class GTKScannerEntry(Gtk.Dialog):
         self.close_button.connect("clicked", self.close_callback)
 
 
-#        try:
-#            self.set_screen(parent.get_screen())
-#            self.connect("destroy", parent.destroy)
-#        except AttributeError:
-#            self.connect("destroy", self.destroy)
-
-#        self.set_title("Enter ISBN")
         self.set_border_width(5)
 
         box = self.get_content_area()
-        # add a vertical box
-#        vbox1 = Gtk.VBox(False, 4)
-#        self.add(vbox1)
 
         # add a label
         label1 = Gtk.Label("Enter ISBN:")
@@ -47,21 +37,8 @@ class GTKScannerEntry(Gtk.Dialog):
         box.pack_start(self.isbn_entry,
             expand=False, fill=False, padding=0)
 
-        # add a quit button
-#        self.quit_button = Gtk.Button("Quit")
-#        self.quit_button.connect_object("clicked",
-#            Gtk.Widget.destroy, self)
-#        box.pack_start(self.quit_button,
-#            expand=False, fill=False, padding=0)
-
         # show stuff
         self.show_all()
-
-#    def destroy(self, widget, data=None):
-#        if self.library_page != None:
-#            print("Saving...", end=' ')
-#            self.library_page.save_to_file()
-#        Gtk.main_quit()
 
     def isbn_enter_callback(self, widget, entry):
         """Query the web searchers for the ISBN, add the book to the library, clear the text field."""
